52a6b3a51a2cffdbdbebe5c16445fe6944b58faa
AbhishekM1295 <118669522+AbhiMAtGitHub@users.noreply.github.com>
2024-02-27 00:31:01 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.mysql"),
-#         "NAME": os.environ.get("DB_NAME", "predictdb"),
-#         "USER": os.environ.get("DB_USER", "root"),
-#         "PASSWORD": os.environ.get("DB_PASSWORD", "root"),
-#         "HOST": os.environ.get("DB_HOST", "mysql"),
-#         "PORT": os.environ.get("DB_PORT", "3306"),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.mysql"),
+        "NAME": os.environ.get("DB_NAME", "predictdb"),
+        "USER": os.environ.get("DB_USER", "root"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "root"),
+        "HOST": os.environ.get("DB_HOST", "mysql"),
+        "PORT": os.environ.get("DB_PORT", "3306"),
+    }
+}
 
 # DATABASES = {
 #     'default': {
